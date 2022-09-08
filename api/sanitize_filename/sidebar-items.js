826dfff3327dd31e2442b330dc5b94a6bedfcf0d
charles-r-earp <charles.r.earp@gmail.com>
2022-09-08 01:42:48 +0000

    Deploying to gh-pages from  @ e9cdc7de514ec9813510542c92634d7883facbfd 🚀

--- api/sanitize_filename/sidebar-items.js
+++ api/sanitize_filename/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["sanitize",""],["sanitize_with_options",""]],"struct":[["Options",""]]});
+window.SIDEBAR_ITEMS = {"fn":[["is_sanitized",""],["is_sanitized_with_options",""],["sanitize",""],["sanitize_with_options",""]],"struct":[["Options",""],["OptionsForCheck",""]]};
